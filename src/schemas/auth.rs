9c3ab5ef76b8a30246b3b86903ab14a8314d3b1b
MrlingXD <wlingzhenyu@163.com>
2025-07-27 18:46:34 +0800

    fix: 修正 AuthToken 结构体的注释和示例值

--- src/schemas/auth.rs
+++ src/schemas/auth.rs
@@ -3,10 +3,12 @@ use utoipa::ToSchema;
 
 #[derive(Debug, Clone, Serialize, Deserialize, ToSchema)]
 pub struct AuthToken {
-    /// 服务器列表
-    #[schema(example = "fasdfashfksdf")]
+    /// JWT 访问令牌
+    #[schema(
+        example = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyLCJleHAiOjE1MTYyNDI2MjJ9.SflKxwRJSMeKKF2QT4fwpMeJf36POk6yJV_adQssw5c"
+    )]
     pub access_token: String,
     /// 过期时间
-    #[schema(example = 3600)]
+    #[schema(example = 2592000)]
     pub expires_in: u64,
 }
