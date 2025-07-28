42b6e66bd3d6813aa22cef843412c10d4a2f328f
MrlingXD <wlingzhenyu@163.com>
2025-07-28 16:35:39 +0800

    refactor(email): 优化邮件构建逻辑，添加发件人信息

--- src/services/email/sender.rs
+++ src/services/email/sender.rs
@@ -1,6 +1,7 @@
 use crate::config::Config;
 use anyhow::{Context, Result};
 use lettre::message::header::ContentType;
+use lettre::message::Mailbox;
 use lettre::transport::smtp::authentication::Credentials;
 use lettre::Message;
 use lettre::SmtpTransport;
@@ -8,7 +9,10 @@ use lettre::SmtpTransport;
 /// 构建邮件消息
 pub fn build_email_message(to_email: &str, from_email: &str, body: String) -> Result<Message> {
     Message::builder()
-        .from(from_email.parse().context("解析发件人邮箱地址失败")?)
+        .from(Mailbox::new(
+            Some("MSCPO 验证系统".to_string()),
+            from_email.parse().context("解析发件人邮箱地址失败")?,
+        ))
         .to(to_email.parse().context("解析收件人邮箱地址失败")?)
         .subject("邮箱验证码")
         .header(ContentType::TEXT_HTML)
@@ -18,8 +22,8 @@ pub fn build_email_message(to_email: &str, from_email: &str, body: String) -> Re
 
 /// 构建SMTP传输对象
 pub fn build_smtp_transport(config: &Config) -> Result<SmtpTransport> {
-    let mut builder = SmtpTransport::relay(&config.email.smtp_server)
-        .context("Failed to create SMTP relay")?;
+    let mut builder =
+        SmtpTransport::relay(&config.email.smtp_server).context("Failed to create SMTP relay")?;
     builder = builder.port(config.email.smtp_port);
     Ok(builder
         .credentials(Credentials::new(
